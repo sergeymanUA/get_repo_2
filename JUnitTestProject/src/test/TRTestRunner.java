be8daa15b9491691b01571f716428204bd9aabe3
sergey <sergeyman78@gmail.com>
2017-10-01 11:22:47 +0300

    After a minor update

--- JUnitTestProject/src/test/TRTestRunner.java
+++ JUnitTestProject/src/test/TRTestRunner.java
@@ -4,6 +4,7 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
+/*This is a short comment */
 public class TRTestRunner {
 	public static void main(String[] args) {
 		Result result = JUnitCore.runClasses(TestResultClass.class);
